e74c7a129611873d6cd4b658f4ea9be4563965b0
David Liang <34115092+davidvliang@users.noreply.github.com>
2023-04-20 22:23:49 -1000

    clean code

--- Helpers/getHarmonicCoefficientMatrix.m
+++ Helpers/getHarmonicCoefficientMatrix.m
@@ -1,20 +1,22 @@
 function gamma = getHarmonicCoefficientMatrix(Q,N,L)
 %%% harmonic coefficient matrix corresponding to the modulation function
 %%% equation 18 of Ni et. al. 2021
+%%% 
 %%% Q - harmonic number
 %%% N - number of antenna elements
 %%% L - "ON" time of phase
 
-q = -Q:1:Q; % range of harmonic values
-n = 1:N; % range of antenna elements
-gamma = zeros(length(q),N); % init gamma
 
-for q_ = 1:1:length(q)
-    for n_ = 1:1:length(n)
-        if q_ == 0
-            gamma(q_,n_) = 2*L/N-1;
+q = -Q:1:Q;                     % range of harmonic values
+n = 1:N;                        % range of antenna elements
+gamma = zeros(length(q),N);     % init gamma
+
+for q_idx = 1:1:length(q)
+    for n_idx = 1:1:length(n)
+        if q_idx == 0
+            gamma(q_idx,n_idx) = 2*L/N-1;
         else
-            gamma(q_,n_) = (2*sin(pi*q_*L/N)/(pi*q_))*exp(-1i*pi*q_*((2*n_-2+L)/N));
+            gamma(q_idx,n_idx) = (2*sin(pi*q_idx*L/N)/(pi*q_idx))*exp(-1i*pi*q_idx*((2*n_idx-2+L)/N));
         end
     end
 end
