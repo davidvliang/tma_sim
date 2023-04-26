5df1c1cbece392967355766411da40633a8477f3
David Liang <34115092+davidvliang@users.noreply.github.com>
2023-04-25 22:51:29 -1000

    test he2018 mod function

--- Helpers/getHarmonicCoefficientMatrix.m
+++ Helpers/getHarmonicCoefficientMatrix.m
@@ -6,7 +6,7 @@ function gamma = getHarmonicCoefficientMatrix(Q,N,L)
 %%% N - number of antenna elements
 %%% L - "ON" time of phase
 
-
+% fp = 2e6;           % modulation frequency of RF switches [Hz]
 q = -Q:1:Q;                     % range of harmonic values
 n = 1:N;                        % range of antenna elements
 gamma = zeros(length(q),N);     % init gamma
@@ -20,3 +20,14 @@ for q_idx = 1:1:length(q)
         end
     end
 end
+
+% for q_idx = 1:1:length(q)
+%     for n_idx = 1:1:length(n)
+%         fp = (30+0.5*(n_idx-1))*10^6;
+%         if q_idx == 0
+%             gamma(q_idx,n_idx) = fp*(L/N);
+%         else
+%             gamma(q_idx,n_idx) = (sin(pi*q_idx*fp*L)/(pi*q_idx))*exp(-1i*pi*fp*(L));
+%         end
+%     end
+% end
