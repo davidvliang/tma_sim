d440866da4831bd9da13f577e3fa1039ca5d30f7
David Liang <34115092+davidvliang@users.noreply.github.com>
2023-04-27 20:41:10 -1000

    tried implementing modfreq term

--- Helpers/getSingleChannel.m
+++ Helpers/getSingleChannel.m
@@ -1,17 +1,19 @@
-function Yt = getSingleChannel(fp, gamma, X)
+function Yt = getSingleChannel(fp, Q, Nt, gamma, Xnt)
 %%% Ni 2021 (eq6)
 
 q = -Q:1:Q;                     % range of harmonic values
-n = 1:N;                        % range of antenna elements
-w = zeros(q,1);                 % init modulation term
-Yt = zeros(length(q),N);        % init single channel
+% n = 1:N;                        % range of antenna elements
+t=1:1:Nt;                       % range of snapshots
 
-for q_idx=1:1:length(q_idx)
-    w(q_idx) = exp(-1i*2*pi*q_idx*fp*t);
+w = zeros(Nt, length(q));       % init modulation term
+
+for tdx = 1:1:Nt
+    for qdx = 1:1:length(q)
+        w(tdx,qdx) = exp(-1i*2*pi*q(qdx)*fp*t(tdx));
+    end
 end
 
-
-% Yt = w*gamma*Xnt;             % modulate Xnt and combine
+Yt = (gamma*Xnt)*w;             % modulate Xnt and combine
 
 
 
